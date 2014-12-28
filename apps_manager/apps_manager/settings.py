a29b668f3daadaf7354a062942a34da0e4aa5fed
Jesús Herrera <jesusmaherrera@gmail.com>
2014-12-27 20:33:46 -0700

    add rest framework

--- apps_manager/apps_manager/settings.py
+++ apps_manager/apps_manager/settings.py
@@ -27,8 +27,15 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
+# Application definition
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'applications',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
--- apps_manager/apps_manager/urls.py
+++ apps_manager/apps_manager/urls.py
@@ -1,10 +1,42 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from applications.models import Application
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+class ApplicationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Application
+        fields = ('name', 'description',)
+
+# ViewSets define the view behavior.
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+
+router.register(r'applications', ApplicationViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'apps_manager.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
